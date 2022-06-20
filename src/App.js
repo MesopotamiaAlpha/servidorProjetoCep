c2ec38cf89188c6a8916282e0ae87a148d107226
Jose Antonio <leao-gomes@hotmail.com>
2022-06-20 16:56:22 -0300

    Servidor funcionando

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
+import Axios from 'axios';
+import { ListItem } from '@mui/material';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  const url = "http://172.25.106.222:3002/api";
+
+
+    const [postList, setPostList] = useState([]);
+
+    useEffect((data) => {
+        Axios.get(url + "/get").then((data) => {
+            setPostList(data.data);
+            console.log(data.data);
+        });
+    }, []);
+
+
   return (
     <div className="App">
-      <input type="text" placeholder="Pesquisar" />
+      <input type="text" placeholder="Cadastrar no banco" />
       <div className="Resultados">
         <h2>Campo de resultados do banco de dados</h2>
         <p>Descrição</p>
         <div className='Tabela'>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
-          <p className='texto'>Texto dentro da div <button className='excluir'>X</button></p>
+
+        <div className="resultado">
+        <h1 className="titulo">Prinex recebidos</h1>
+        <ul className="listaUl">
+
+            {postList.map(cep =>
+                <ListItem style={{ backgroundColor: "#594b8f" }} component="a" className="textoLista" key={cep.id}>
+                    {"Id do registro: " + cep.id + "   / Cep pesquisado: " + cep.cep}
+                    <button className='excluir'>X</button>
+                </ListItem>)}
+        </ul>
+    </div>
+          
         </div>
       </div>
     </div>
