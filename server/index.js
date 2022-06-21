a28f0731be41679636d784ddcbc42726c6d9151c
Jose Antonio <leao-gomes@hotmail.com>
2022-06-21 17:59:40 -0300

    Ajuste na UL, retirado

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const db = require('./config/db')
-const cors = require('cors')
+const db = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3002;
